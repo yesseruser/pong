7ffba9ff589e1fd37b27caea403cc3ebfad1fa8e
Yesseruser <matej.petricek@gmail.com>
2021-03-08 17:54:24 +0100

    Added right player controls

--- pong.py
+++ pong.py
@@ -1,12 +1,13 @@
 import pygame
 pygame.init()
-# bla
-window = pygame.display.set_mode((800, 600))
 
-left_pos = 150
+window = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+left_pos = 255
 left_speed = 0
 
-right_pos = 150
+right_pos = 255
 right_speed = 0
 
 run = True
@@ -19,12 +20,29 @@ while run:
                 left_speed = -1
             elif event.key == pygame.K_s:
                 left_speed = +1
+            elif event.key == pygame.K_UP:
+                right_speed = -1
+            elif event.key == pygame.K_DOWN:
+                right_speed = -1
         elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_w or event.key == pygame.K_s:
                     left_speed = 0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    right_speed = 0
 
     left_pos = left_pos + left_speed
+    if left_pos < 0:
+        left_pos = 0
+    if left_pos > 450:
+        left_pos = 450
+    if right_pos < 0:
+        right_pos = 0
+    if right_pos > 450:
+        right_pos = 450
 
     window.fill("black")
     pygame.draw.rect(window, "white", (0, left_pos, 30, 150))
+    pygame.draw.rect(window, "white", (770, right_pos))
     pygame.display.flip()
+
+    clock.tick(60)
